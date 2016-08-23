0f9138a39b26ef7d9730f5c350be8ed80d45464f
AJITPM <ajitcse96@gmail.com>
2016-08-23 21:38:14 +0530

    Create stoi.java

---
+++ stoi.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class posneg {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner (System.in);
+		String c1=(sc.next());
+		int n= Integer.parseInt(c1);
+		System.out.println(n);
+	}
+
+}
